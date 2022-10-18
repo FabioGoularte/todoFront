ee7bed9bb546281e41dd3f71bb4108f682228ecd
sergiomarinellib23 <sergiomarinellib23@gmail.com>
2022-10-18 16:57:57 -0300

    css tailwind

--- todolist/src/pages/tarefas/Tarefas.tsx
+++ todolist/src/pages/tarefas/Tarefas.tsx
@@ -1,33 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import Swal from 'sweetalert2';
-import { API } from '../../api';
+import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+import { API } from "../../api";
 
 const Tarefas = () => {
   type ToDo = {
     id: number;
     title: string;
     completed: boolean;
-  }
+  };
 
-  const [toDos, setToDos] = useState<ToDo[]>([])
-  const [newToDo, setNewToDo] = useState("")
+  const [toDos, setToDos] = useState<ToDo[]>([]);
+  const [newToDo, setNewToDo] = useState("");
 
   useEffect(() => {
     const getToDos = async () => {
       const { data } = await API.get(`/todos`);
-      setToDos(data);
+      sortToDos(data);
     };
     getToDos();
   }, []);
 
   const handleToDoCreate = async () => {
-    const { data } = await API.post('/todos', {
-      title: newToDo
-    })
+    const { data } = await API.post("/todos", {
+      title: newToDo,
+    });
 
     setToDos([data, ...toDos]);
-    console.log(toDos)
-  }
+    console.log(toDos);
+  };
 
   const setDoneToDo = async (id: number) => {
     const { data } = await API.put(`/todos/done`, { id });
@@ -36,18 +36,18 @@ const Tarefas = () => {
         return data;
       }
       return toDo;
-    })
+    });
     sortToDos(newTodos);
-  }
+  };
 
   const setUndoneToDo = async (id: number) => {
     const { data } = await API.put(`/todos/undone`, { id });
-    const newTodos =  toDos.map((toDo) => {
+    const newTodos = toDos.map((toDo) => {
       if (toDo.id === id) {
         return data;
       }
       return toDo;
-    })
+    });
     sortToDos(newTodos);
   };
 
@@ -68,45 +68,46 @@ const Tarefas = () => {
     });
 
     setToDos(sortedToDos);
-  }
-
+  };
 
   const handleAlterTodo = (id: Number) => {
     Swal.fire({
-      title: 'Alterar todo',
-      input: 'text',
+      title: "Alterar todo",
+      input: "text",
       inputAttributes: {
-        autocapitalize: 'off'
+        autocapitalize: "off",
       },
       showCancelButton: true,
-      confirmButtonText: 'confirmar',
+      confirmButtonText: "confirmar",
       showLoaderOnConfirm: true,
       preConfirm: async (title) => {
-        const { data } = await API.put('/todos', {title, id});
+        const { data } = await API.put("/todos", { title, id });
         const newTodos = toDos.map((toDo) => {
           if (toDo.id === id) {
             return data;
           }
           return toDo;
-        })
+        });
         sortToDos(newTodos);
       },
-      allowOutsideClick: () => !Swal.isLoading()
+      allowOutsideClick: () => !Swal.isLoading(),
     }).then((result) => {
       if (result.isConfirmed) {
         Swal.fire({
           title: `O todo foi alterado`,
-          icon: "success"
-        })
+          icon: "success",
+        });
       }
-    })
-  }
+    });
+  };
 
   return (
     <div className="w-full flex justify-center bg-teal-lightest font-sans bg-purple-700 min-h-screen">
       <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg h-min">
         <div className="mb-4">
-          <h1 className="text-grey-darkest">Todo List</h1>
+          <h1 className="text-grey-darkest text-xl font-bold text-purple-800">
+            Todo List
+          </h1>
           <div className="flex mt-4">
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
@@ -114,42 +115,69 @@ const Tarefas = () => {
               value={newToDo}
               onChange={(e) => setNewToDo(e.target.value)}
             />
-            <button className="flex-no-shrink p-2 border-2 rounded text-teal-600 border-teal-600 hover:text-white hover:bg-teal-600" onClick={handleToDoCreate}>Add</button>
+            <button
+              className="flex-no-shrink p-2 border-2 rounded text-teal-600 border-teal-600 hover:text-white hover:bg-teal-600"
+              onClick={handleToDoCreate}
+            >
+              Add
+            </button>
           </div>
         </div>
         <div>
-        
-        { toDos.map((toDo) => (
-              !toDo.completed ? (
-              <div className="flex mb-4 items-center" key={toDo.id} onClick={(e) => {e.preventDefault(); handleAlterTodo(toDo.id);}}>
-                  <p className="w-full text-grey-darkest">
-                    {toDo.title}
-                  </p>
-                  <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green-600 border-green-600 hover:bg-green-600" onClick={() => setDoneToDo(toDo.id)}>
-                    Done
-                  </button>
-                  <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red-600 border-red-600 hover:text-white hover:bg-red-600" onClick={(e) => {handleToDoDelete(toDo.id)}}>
-                    Remove
-                  </button>
+          {toDos.map((toDo) =>
+            !toDo.completed ? (
+              <div
+                className="flex mb-4 items-center cursor-pointer hover:bg-gray-100 rounded-md p-4"
+                key={toDo.id}
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleAlterTodo(toDo.id);
+                }}
+              >
+                <p className="w-full text-grey-darkest">{toDo.title}</p>
+                <button
+                  className="flex-no-shrink  w-80 p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green-600 border-green-600 hover:bg-green-600"
+                  onClick={() => setDoneToDo(toDo.id)}
+                >
+                  Done
+                </button>
+                <button
+                  className="flex-no-shrink p-2 ml-2 border-2 rounded text-red-600 border-red-600 hover:text-white hover:bg-red-600"
+                  onClick={(e) => {
+                    handleToDoDelete(toDo.id);
+                  }}
+                >
+                  Remove
+                </button>
               </div>
-              ) : (
-                <div className="flex mb-4 items-center" key={toDo.id}>
-                  <p className="w-full line-through text-green-600">
-                    {toDo.title}
-                  </p>
-                  <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-gray-600 border-gray-300 hover:bg-gray-300" onClick={(e) => {setUndoneToDo(toDo.id)}}>
-                    Not Done
-                  </button>
-                  <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red-600 border-red-600 hover:text-white hover:bg-red-600" onClick={(e) => {handleToDoDelete(toDo.id)}}>
-                    Remove
-                  </button>
-                </div>
-              )))}
-          
+            ) : (
+              <div className="flex mb-4 items-center p-4" key={toDo.id}>
+                <p className="w-full line-through text-green-600">
+                  {toDo.title}
+                </p>
+                <button
+                  className="flex-no-shrink w-80 p-2 ml-4 mr-2 border-2 rounded hover:text-white text-gray-600 border-gray-300 hover:bg-gray-300"
+                  onClick={(e) => {
+                    setUndoneToDo(toDo.id);
+                  }}
+                >
+                  Not Done
+                </button>
+                <button
+                  className="flex-no-shrink p-2 ml-2 border-2 rounded text-red-600 border-red-600 hover:text-white hover:bg-red-600"
+                  onClick={(e) => {
+                    handleToDoDelete(toDo.id);
+                  }}
+                >
+                  Remove
+                </button>
+              </div>
+            )
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Tarefas
+export default Tarefas;
